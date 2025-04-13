d5bad04bb61cb1639170f6d92b6f27e19c7a0230
MICHEE CEPHAS <123488406+skyson-ai@users.noreply.github.com>
2025-04-13 23:08:06 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "",
+  title: "Cephas Portofolio",
   description: "Backend Developer & ML Enthusiast Portfolio",
     generator: 'nextjs.dev'
 }
